14a9ee6fbd0358d006737fe4aefd73a091878457
chileangoblin <chileangoblin@gmail.com>
2025-06-18 16:23:15 -0400

    🍄

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,6 @@
 export default async function(eleventyConfig) {
+    eleventyConfig.addWatchTarget("./src/sass");
+    eleventyConfig.addGlobalData("permalink", "{{ page.filePathStem }}.html");
     eleventyConfig.setInputDirectory("src");
     eleventyConfig.setOutputDirectory("dist");
 }
